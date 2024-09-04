6870374a9f8e004b355f38ff9ab0fde71c59a26f
praveenraam <ckpraveenraam@gmail.com>
2024-09-04 12:40:14 +0530

    Basic Layout Styling was done, Function coding was modified

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -1,4 +1,4 @@
-import { Pressable, Text, View } from 'react-native';
+import { Pressable, Text, View, Image } from 'react-native';
 import React, { useState } from 'react';
 import { Parser } from 'expr-eval';
 
@@ -13,14 +13,13 @@ const Calculator = () => {
     const calculateResult = () => {
         try{
             const result = parser.evaluate(val);
+            updateHistory('(' + val + ') = ' + result + '   ');
             setVal(result.toString());
-            updateHistory(result + ' ');
         }
         catch(error){
             if (error instanceof Error) {
                 displayError(error);
             } else {
-                // Handle unexpected types if needed
                 displayError(new Error('Unknown error occurred'));
             }
         }
@@ -31,43 +30,83 @@ const Calculator = () => {
         setHistory(newHistory);
     };
 
+    const deleteLastChar = () => {
+        setVal(val.slice(0,-1));
+    };
+
     const displayError = (error: Error) =>{
         setVal('Error ' + error);
-        setTimeout(() => setVal(' '),10000);
+        setTimeout(() => setVal(' '),3000);
+    };
+
+    const resetVal = () => {
+        setVal('');
     };
 
     return (
-        <View>
+        <View className="grid grid-rows-4 grid-flow-row gap-10">
             <View>
                 <View>
                     <Text>{history}</Text>
                 </View>
                 <Text>{val}</Text>
             </View>
-            <View>
-                <View>
+            <View className="grid grid-rows-4 grid-flow-row gap-4 ">
+
+
+                <View className="flex flex-row justify-center items-center mx-auto">
+                    <Pressable className="py-5 mx-3.5 w-44 h-16 bg-red-400 rounded-full" onPress={() => resetVal()}>
+                        <Text className="text-center text-white">Reset</Text>
+                    </Pressable>
+                    <Pressable className="m-3 w-16 h-16 bg-gray-500 rounded-full flex items-center justify-center" onPress={() => handlePress('(')}>
+                        <Text className="text-white text-lg">(</Text>
+                    </Pressable>
+                    <Pressable className="m-3 w-16 h-16 bg-gray-500 rounded-full flex items-center justify-center" onPress={() => handlePress(')')}>
+                        <Text className="text-white text-lg">)</Text>
+                    </Pressable>
+                </View>
+
+                <View className="flex flex-row justify-center items-center mx-auto ">
                     {['1','2','3','+'].map((char) => (
-                        <Pressable key={char} onPress={() => handlePress(char)}><Text>{char}</Text></Pressable>
-                    ))}
+                        <Pressable
+                            key={char}
+                            className="m-4 w-16 h-16 bg-gray-500 rounded-full flex items-center justify-center"
+                            onPress={() => handlePress(char)}
+                        >
+                            <Text className="text-white text-lg">{char}</Text>
+                        </Pressable>                    ))}
                 </View>
 
-                <View>
+                <View className="flex flex-row justify-center items-center mx-auto ">
                     {['4','5','6','-'].map((char) => (
-                        <Pressable key={char} onPress={() => handlePress(char)}><Text>{char}</Text></Pressable>
+                        <Pressable
+                            key={char}
+                            className="m-4 w-16 h-16 bg-gray-500 rounded-full flex items-center justify-center"
+                            onPress={() => handlePress(char)}
+                        >
+                            <Text className="text-white text-lg">{char}</Text>
+                        </Pressable>
                     ))}
                 </View>
 
-                <View>
+                <View className="flex flex-row justify-center items-center mx-auto ">
                     {['7','8','9','*'].map((char) => (
-                        <Pressable key={char} onPress={() => handlePress(char)}><Text>{char}</Text></Pressable>
-                    ))}
+                        <Pressable
+                            key={char}
+                            className="m-4 w-16 h-16 bg-gray-500 rounded-full flex items-center justify-center"
+                            onPress={() => handlePress(char)}
+                        >
+                            <Text className="text-white text-lg">{char}</Text>
+                        </Pressable>                    ))}
                 </View>
 
-                <View>
-                    <Pressable onPress={()=>setVal('')}><Text>Reset</Text></Pressable>
-                    <Pressable onPress={()=>handlePress('0')}><Text>0</Text></Pressable>
-                    <Pressable onPress={()=>calculateResult()}><Text>=</Text></Pressable>
-                    <Pressable onPress={()=>handlePress('/')}><Text>/</Text></Pressable>
+                <View className="flex flex-row justify-center items-center mx-auto ">
+                    <Pressable onPress={deleteLastChar} className="m-4 w-16 h-16 bg-gray-500 rounded-full flex items-center justify-center">
+                        <Image source={require('./img/backspace.png')} className="w-5 h-5"  />
+                    </Pressable>
+                    <Pressable className="m-4 w-16 h-16 bg-gray-500 rounded-full flex items-center justify-center"onPress={()=>handlePress('0')}><Text className="text-white text-lg" >0</Text></Pressable>
+                    <Pressable className="m-4 w-16 h-16 bg-gray-500 rounded-full flex items-center justify-center" onPress={()=>calculateResult()}><Text className="text-white text-lg" >=</Text></Pressable>
+                    <Pressable className="m-4 w-16 h-16 bg-gray-500 rounded-full flex items-center justify-center" onPress={()=>handlePress('/')}><Text className="text-white text-lg">/</Text></Pressable>
                 </View>
             </View>
         </View>

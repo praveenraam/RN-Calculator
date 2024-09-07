2706c8f8d2ad6d05a2b25b51cdcf33b7e8297b41
praveenraam <ckpraveenraam@gmail.com>
2024-09-07 18:06:15 +0530

    #5 Code for storing history is started

---
+++ src/asyncStorageHelper.ts
@@ -0,0 +1,31 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export const saveToAsyncStorage = async(key:string, value:any): Promise<void> => {
+    try{
+        const jsonValue = JSON.stringify(value);
+        await AsyncStorage.setItem(key,jsonValue);
+    }
+    catch(error){
+        console.log('Error in Saving the data',error);
+    }
+};
+
+export const getFromAsyncStorage = async (key:string): Promise<any | null> => {
+    try{
+        const jsonValue = AsyncStorage.getItem(key);
+        return jsonValue != null ? jsonValue : null;
+    }
+    catch(error){
+        console.log('Error retrieving Data',error);
+        return null;
+    }
+};
+
+export const removeFromAsyncStorage = async (key:string): Promise<void> => {
+    try{
+        await AsyncStorage.removeItem(key);
+    }
+    catch(error){
+        console.log('Error in removing the data',error);
+    }
+};

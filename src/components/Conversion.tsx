40fb79ca8806418c60fcf43400680ec3524cb8f9
praveenraam <ckpraveenraam@gmail.com>
2024-09-11 09:17:43 +0530

    #13, Styling for the button is done

---
+++ src/components/Conversion.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+import React from 'react';
+
+const Conversion = () => {
+  return (
+    <View>
+      <Text>Conversion</Text>
+    </View>
+  );
+};
+
+export default Conversion;
--- src/screens/ConversionsList.tsx
+++ src/screens/ConversionsList.tsx
@@ -1,10 +1,33 @@
-import { Text, View } from 'react-native';
 import React from 'react';
+import { Text, View, FlatList, Pressable, Image } from 'react-native';
+
+type ConversionType = {
+  id: string;
+  name: string;
+};
+
+const conversions: ConversionType[] = [
+  { id: '1', name: 'Temperature' }, // Fixed spelling
+  { id: '2', name: 'Weight' },
+  { id: '3', name: 'Length' },
+  { id: '4', name: 'Area' },
+];
 
 const ConversionsList = () => {
+  const renderItem = ({ item }: { item: ConversionType }) => (
+    <Pressable className="p-4 bg-gray-700 rounded-md my-2 flex-row item-center">
+      <Text className="text-white text-lg flex-1">{item.name}</Text>
+      <Image source={require('../img/right.png')} className="w-5 h-5" />
+    </Pressable>
+  );
+
   return (
-    <View>
-      <Text>ConversionsList</Text>
+    <View className="flex-1 bg-black p-4">
+      <FlatList
+        data={conversions}
+        keyExtractor={(item) => item.id}
+        renderItem={renderItem}
+      />
     </View>
   );
 };

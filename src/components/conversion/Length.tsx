8df12addc8b4908ff74642849cf675bdae5a2dff
praveenraam <ckpraveenraam@gmail.com>
2024-09-14 12:44:48 +0530

    #19, Code for the basic Length component and styling were done

--- src/components/conversion/Length.tsx
+++ src/components/conversion/Length.tsx
@@ -1,10 +1,63 @@
 import { Text, View } from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Picker } from '@react-native-picker/picker';
 
-const Length = () => {
+type LengthProps = {
+  inputValue: string;
+};
+
+const Length = ({ inputValue }: LengthProps) => {
+  const [resultValue, setResultValue] = useState<string>('');
+  const [conversionType, setConversionType] = useState<string>('Meters to Centimeters');
+
+  // Conversion logic for length
+  const lengthConversionFunc = (value: number, type: string) => {
+    switch (type) {
+      case 'Meters to Centimeters':
+        return (value * 100).toFixed(6); // 1 meter = 100 centimeters
+      case 'Centimeters to Meters':
+        return (value / 100).toFixed(6); // 1 centimeter = 0.01 meters
+      default:
+        return '0';
+    }
+  };
+
+  useEffect(() => {
+    if (inputValue) {
+      const numericValue = parseFloat(inputValue);
+      if (!isNaN(numericValue)) {
+        const convertedValue = lengthConversionFunc(numericValue, conversionType);
+        setResultValue(convertedValue);
+      } else {
+        setResultValue('');
+      }
+    } else {
+      setResultValue('');
+    }
+  }, [inputValue, conversionType]);
+
   return (
-    <View className="h-1/4">
-      <Text className="text-white">Length</Text>
+    <View className="flex flex-col justify-center items-center">
+
+      <View className="h-12 bg-gray-800 rounded-lg justify-center items-center mb-3">
+        <Picker
+          dropdownIconColor={'#fff'}
+          selectedValue={conversionType}
+          style={{ height: 50, width: 250, color: 'white' }}
+          onValueChange={(itemValue) => setConversionType(itemValue)}
+        >
+          <Picker.Item label="Meters to Centimeters" value="Meters to Centimeters" />
+          <Picker.Item label="Centimeters to Meters" value="Centimeters to Meters" />
+        </Picker>
+      </View>
+
+      <View className="bg-gray-800 w-4/5 p-4 my-3 rounded-lg">
+        <Text className="text-white text-2xl text-center">{inputValue}</Text>
+      </View>
+
+      <View className="bg-gray-800 w-4/5 p-4 my-3 rounded-lg">
+        <Text className="text-white text-2xl text-center">{resultValue}</Text>
+      </View>
     </View>
   );
 };
